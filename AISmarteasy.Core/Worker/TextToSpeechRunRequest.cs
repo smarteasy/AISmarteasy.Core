d2b7b504aa1ebb37e76ec418f1982b43db5e7c90
smarteasy <kimhn@umlcert.com>
2024-01-05 18:59:34 +0900

    음성 지원 최종 테스트

--- AISmarteasy.Core/Worker/TextToSpeechRunRequest.cs
+++ AISmarteasy.Core/Worker/TextToSpeechRunRequest.cs
@@ -2,6 +2,10 @@
 
 public readonly struct TextToSpeechRunRequest(string speechFilePath, string voice)
 {
+    public TextToSpeechRunRequest(string voice) : this(string.Empty, voice)
+    {
+    }
+
     public string Voice{ get; } = voice;
     
     public string SpeechFilePath { get; } = speechFilePath;
